54367b4ad58a59539ce532c7bb478f2f978e32e1
smilemask92 <rhkwk3333@naver.com>
2022-10-21 20:12:37 +0900

    feat error fix

--- pinteresting/pinteresting/urls.py
+++ pinteresting/pinteresting/urls.py
@@ -18,13 +18,15 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from articleapp.views import ArticleListView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include("accountapp.urls")),
     path('profiles/',include("profileapp.urls")),
     path('articles/',include('articleapp.urls')),
     path('comments/',include('commentapp.urls')),
-    path('',include("articleapp.urls",namespace='default')),
+    path('',ArticleListView.as_view(),name='home'),
     path('projects/',include('projectapp.urls')),
     path('subscribes/', include('subscribeapp.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
